647f4b0b780dd0adff158700c9803b73387eca2a
nguyenvanthanh0055-cyber <nguyenvanthanh0055@gmail.com>
2026-06-09 17:23:33 +0700

    fix: update command config
    Ensure KAFKA_BOOTSTRAP_SERVERS is mandatory and set default CLIENT_CONFIG.

--- scripts/create_kafka_topic.sh
+++ scripts/create_kafka_topic.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 TOPIC_NAME="${KAFKA_TOPIC:-inventory-events}"
-BOOTSTRAP_SERVERS="${KAFKA_BOOTSTRAP_SERVERS}"
+BOOTSTRAP_SERVERS="${KAFKA_BOOTSTRAP_SERVERS:?KAFKA_BOOTSTRAP_SERVERS is required}"
+CLIENT_CONFIG="${CLIENT_CONFIG:-$HOME/msk-config/client.properties}"
 
 kafka-topics.sh \
   --bootstrap-server "$KAFKA_BOOTSTRAP_SERVERS" \
-  --command-config client.properties \
+  --command-config "${CLIENT_CONFIG}" \
   --create \
   --if-not-exists \
   --topic "$KAFKA_TOPIC" \
